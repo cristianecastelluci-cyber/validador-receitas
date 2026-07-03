1cc20651a3b9b41affdb4ecfb69f54c73b7cb218
cristianecastelluci-cyber <cristianecastelluci@gmail.com>
2026-07-03 20:33:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-document.getElementById("receita").onclick = async () => {
 
-const file = document.getElementById("upload").files[0];
-
-if (!file) {
-alert("Envie uma imagem da receita primeiro!");
-return;
-}
-
-document.getElementById("resultado").innerText = "Lendo receita...";
-
-const reader = new FileReader();
-
-reader.onload = async function () {
-
-const result = await Tesseract.recognize(
-reader.result,
-'por'
-);
-
-const texto = result.data.text;
-
-document.getElementById("resultado").innerText = texto;
-
-analisarMedicamentos(texto);
-
+document.getElementById("btnCamera").onclick = () => {
+    document.getElementById("upload").click();
 };
 
-reader.readAsDataURL(file);
+document.getElementById("upload").onchange = async (event) => {
 
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    document.getElementById("resultado").innerText = "Lendo receita...";
+
+    const reader = new FileReader();
+
+    reader.onload = async function () {
+
+        const result = await Tesseract.recognize(
+            reader.result,
+            'por'
+        );
+
+        const texto = result.data.text;
+
+        document.getElementById("resultado").innerText = texto;
+    };
+
+    reader.readAsDataURL(file);
 };
-
-function analisarMedicamentos(texto) {
-
-let medicamentos = ["losartana", "metformina", "amoxicilina", "dipirona"];
-
-let encontrados = medicamentos.filter(m =>
-texto.toLowerCase().includes(m)
-);
-
-alert("Medicamentos encontrados: " + encontrados.join(", "));
-
-}
